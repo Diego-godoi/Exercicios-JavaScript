7bf4370f57ead5acd976fa492613e87457a69d5f
Diego Godoi Martins <diegogodoimartins.11@gmail.com>
2025-01-17 15:13:41 -0300

    refiz o codigo

--- pacote-download/Questao10.js
+++ pacote-download/Questao10.js
@@ -7,14 +7,14 @@
 let produtos = [ { id:1 , nome: "Camiseta", preco: 50, estoque: 10 }, { id: 2, nome: "Calça", preco: 100, estoque: 5 }, { id: 3, nome: "Sapato", preco: 200, estoque: 2 } ];
 
 
-function buscarProdutoPorNome(nome) {
+function buscar(nome) {
     for(i=0;i<produtos.length;i++){
-        if(produtos.nome == nome){
+        if(produtos[i].nome === nome){
             document.write("Detalhes do produto");
-            document.write("Id do produto: "+ produtos.id);
-            document.write("Nome: "+ produtos.nome);
-            document.write("Preco: "+ produtos.preco);
-            document.write("Estoque: "+ produtos.estoque);
+            document.write("Id do produto: "+ produtos[i].id);
+            document.write("Nome: "+ produtos[i].nome);
+            document.write("Preco: "+ produtos[i].preco);
+            document.write("Estoque: "+ produtos[i].estoque);
 
         }else{
             document.write("Produto nao encontrado")
@@ -22,18 +22,28 @@ function buscarProdutoPorNome(nome) {
     }
 }
 
-function adicionarEstoque(id, quantidade){
+function adicionar(id, quantidade){
     for(i=0;i<produtos.length;i++){
-        if(produtos.id == id){
+        if(produtos[i].id === id){
             produtos[i].estoque += quantidade;
         }
     }
 }
 
-function calcularTotalEstoque(){
+function valorEstoque(){
+    let somaPrecos =0;
+    let somaEstoque =0;
     for(i=0;i<produtos.length;i++){
         somaPrecos += produtos[i].preco;
         somaEstoque += produtos[i].estoque;
     }
-    document.write("Valor total em estoque da loja: "+ (somaPrecos*somaEstoque));
+    return somaPrecos*somaEstoque;
 }
+
+let nome = prompt("Digite o nome do produto a ser buscado: ");
+buscar(nome);
+let id = parseInt(prompt("Digite o id do produto para adicionar ao estoque: "));
+let quantidade = parseInt(prompt("Quantidade a ser adicionada: "));
+adicionar(id, quantidade);
+let totalValor = valorEstoque();
+document.write("Valor total em estoque da loja: "+totalValor);
